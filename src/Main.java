f5e2fbc34108397ca4d2c2339f8ad2e8004d71cb
shawn-grant <shawngrant333@gmail.com>
2022-03-23 15:32:39 -0500

    added a UI class

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,12 @@ public class Main {
     static LinkedList customerlList = new LinkedList();;
 
     public static void main(String[] args) {
-        
+        UI.clearScreen(); // clear the screen
+        UI.titleCard(); // show a title card
+
 	    Customer cus1 = new Customer(20, "Don", "Gang", "Male", 1001, 1);
         System.out.println(cus1);
 
-        customerlList.insert(new Customer());
+        customerlList.insert(new Customer());   
     }
 }
---
+++ src/UI.java
@@ -0,0 +1,27 @@
+public class UI {
+    public static final String RESET = "\033[0m";  // Text Reset
+
+    // Regular Colors
+    public static final String BLACK = "\033[0;30m";   // BLACK
+    public static final String RED = "\033[0;31m";     // RED
+    public static final String GREEN = "\033[0;32m";   // GREEN
+    public static final String YELLOW = "\033[0;33m";  // YELLOW
+    public static final String BLUE = "\033[0;34m";    // BLUE
+    public static final String PURPLE = "\033[0;35m";  // PURPLE
+    public static final String CYAN = "\033[0;36m";    // CYAN
+    public static final String WHITE = "\033[0;37m"; // WHITE
+    
+    public static void titleCard() {
+        System.out.println(BLUE + "\n NMIA");
+        resetColor();
+    }
+    
+    public static void resetColor() {
+        System.out.println(RESET);
+    }
+
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+}

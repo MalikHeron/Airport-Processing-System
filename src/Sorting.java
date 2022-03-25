446cd204a88097af065a2ede5813127630c2c590
thiam678 <91034930+thiam678@users.noreply.github.com>
2022-03-24 23:43:35 -0500

    Update Sorting.java

--- src/Sorting.java
+++ src/Sorting.java
@@ -25,5 +25,31 @@ public class Sorting {
 	    	       }
 	    	   }
 	    	}
+	     public void sortByTime(){
+	    	   Node currNode = this.head, index = null;
+	    	   Customer temp;
+
+	    	   if(this.head == null)
+	    	       return;
+	    	   else {
+	    	       while (currNode != null){
+	    	           //Node index will point to node next to currNode
+	    	           index = currNode.next;
+
+	    	           while (index != null){
+	    	               //If current node data is greater than index's node data,
+	    	               //swap the data between them
+	    	               if(currNode.data.getTime() > index.data.getTime()){
+	    	                   temp = currNode.data;
+	    	                   currNode.data = index.data;
+	    	                   index.data = temp;
+	    	               }
+
+	    	               index = index.next;
+	    	           }
+	    	           currNode = currNode.next;
+	    	       }
+	    	   }
+	    	}
 
 }

c409ad72dbff3ba77ff857c7aed4eb9d860ccfee
thiam678 <91034930+thiam678@users.noreply.github.com>
2022-03-24 23:45:52 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,7 @@ public class Main {
         System.out.println();
         
         customerList.sortByPriority();
+        customerList.sortByTime();
         
         customerList.print();
         //customerList.removeById(22);

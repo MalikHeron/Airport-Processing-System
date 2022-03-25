fc5f09cac78e535775c1fe2fcf0fb0adbcb801a3
thiam678 <91034930+thiam678@users.noreply.github.com>
2022-03-24 23:45:01 -0500

    Update LinkedList.java

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -90,6 +90,32 @@ public class LinkedList {
 	    	       }
 	    	   }
 	    	}
+	  public void sortByTime(){
+	    	   Node currNode = this.head, index = null;
+	    	   Customer temp;
+
+	    	   if(this.head == null)
+	    	       return;
+	    	   else {
+	    	       while (currNode != null){
+	    	           //Node index will point to node next to currNode
+	    	           index = currNode.next;
+
+	    	           while (index != null){
+	    	               //If current node data is greater than index's node data,
+	    	               //swap the data between them
+	    	               if(currNode.data.getTime() > index.data.getTime()){
+	    	                   temp = currNode.data;
+	    	                   currNode.data = index.data;
+	    	                   index.data = temp;
+	    	               }
+
+	    	               index = index.next;
+	    	           }
+	    	           currNode = currNode.next;
+	    	       }
+	    	   }
+	    	}
 
     // Method to print the LinkedList.
     public void print() {

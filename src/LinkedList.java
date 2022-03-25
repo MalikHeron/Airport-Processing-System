b458c22c5624c31fb68005b91173f88cf16f2acd
thiam678 <91034930+thiam678@users.noreply.github.com>
2022-03-24 23:34:11 -0500

    Update LinkedList.java

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -63,6 +63,33 @@ public class LinkedList {
 
         System.out.println();
     }
+    
+     public void sortByPriority(){
+	    	   Node currNode = this.head, index = null;
+	    	   Customer temp;
+
+	    	   if(this.head == null)
+	    	       return;
+	    	   else {
+	    	       while (currNode != null){
+	    	           //Node index will point to node next to currNode
+	    	           index = currNode.next;
+
+	    	           while (index != null){
+	    	               //If current node data is greater than index's node data,
+	    	               //swap the data between them
+	    	               if(currNode.data.getPriorityNo() > index.data.getPriorityNo()){
+	    	                   temp = currNode.data;
+	    	                   currNode.data = index.data;
+	    	                   index.data = temp;
+	    	               }
+
+	    	               index = index.next;
+	    	           }
+	    	           currNode = currNode.next;
+	    	       }
+	    	   }
+	    	}
 
     // Method to print the LinkedList.
     public void print() {
